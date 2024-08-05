43fe18f3ed6e2d135fe6c64aadc6d990c275334e
Avery K. <114759302+AveryLebene@users.noreply.github.com>
2024-08-05 14:33:27 +0000

    "project set up"

---
+++ app/components/HeroSection.tsx
@@ -0,0 +1,9 @@
+const HeroSection = () => {
+    return(
+        <div className="">
+        <h2 className="text-8xl"> Hi, I'm Avery a web developer with a passion for user experience </h2>
+        </div>
+    )
+}
+
+export default HeroSection;
